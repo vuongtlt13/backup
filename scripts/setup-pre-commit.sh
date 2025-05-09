ef8fd8c6f746308118f0efc1e45e0d9dc9bf59a7
Đỗ Quốc Vương <vuong.dq@teko.vn>
2025-05-09 09:20:26 +0700

    Fix ci for linting

--- scripts/setup-pre-commit.sh
+++ scripts/setup-pre-commit.sh
@@ -9,7 +9,7 @@ fi
 # Install golangci-lint if not already installed
 if ! command -v golangci-lint &> /dev/null; then
     echo "Installing golangci-lint..."
-    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.55.2
+    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.54.2
 fi
 
 # Install pre-commit hooks
